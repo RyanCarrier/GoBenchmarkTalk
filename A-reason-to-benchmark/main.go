d6354a1ade93fc33f7ed26548e4511efa6753118
rcarrier <rcarrier@byteback.com.au>
2016-05-23 11:47:23 +1000

    lmao first part started forgott o commit last ntight

---
+++ A-reason-to-benchmark/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+const part1 = false
+
+//Set has a set of int which can be set and reset
+type Set []int
+
+func main() {
+	set := Set{1, 2, 3, 4}
+	fmt.Println("set:\n", set)
+	set = set[0:3]
+	fmt.Println("set after reslice:\n", set)
+	//fmt.Println(set[3])
+	clone := set[0:4]
+	clone2 := set
+	clone3 := clone2[0:4]
+	//fmt.Println("set index 3\n", len(set), cap(set), set[3])
+	fmt.Println("set index 3\n", len(set), cap(set))
+	fmt.Println("clone index 3\n", len(clone), cap(clone), clone[3])
+	//fmt.Println("clone2 index 3\n", len(clone2), cap(clone2), clone2[3])
+	fmt.Println("clone2 index 3\n", len(clone2), cap(clone2))
+	fmt.Println("clone3 index 3\n", len(clone3), cap(clone3), clone3[3])
+
+}
+
+//https://github.com/golang/go/wiki/SliceTricks
